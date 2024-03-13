d5d9e28698d4a6d7b0866eb6436fcfcbc5312070
Miguel Fernando garcia Salazar <migeru_garcia@hotmail.com>
2024-03-13 16:32:36 -0600

    Update MgSoftDev.Returning.NotifyExtensions to 1.1.0
    Took 18 minutes

---
+++ MgSoftDev.Returning.NotifyExtensions/NotifyExtension.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using MgSoftDev.Controls.WPF.Notification;
+using MgSoftDev.ReturningCore.Helper;
+
+namespace MgSoftDev.ReturningCore.NotifyExtensions;
+
+public static class NotifyExtension
+{
+    public static Returning<T> SendNotifyIfNotOk<T>(this Returning<T> value, string? messageIfError = null)
+    {
+        SendNotify(value, messageIfError);
+        return value;
+    }
+    public static Returning SendNotifyIfNotOk(this Returning value, string? messageIfError = null)
+    {
+        SendNotify(value, messageIfError);
+        return value;
+    }
+
+    static void SendNotify(Returning value, string? messageIfError)
+    {
+        try
+        {
+            messageIfError ??= "Ocurrió un error intente nuevamente";
+            if (value.ResultType is ReturningBase.TypeResult.Unfinished or ReturningBase.TypeResult.ErrorAndUnfinished)
+            {
+                var uf = value.UnfinishedInfo;
+
+                var title =  uf.Title.Format(uf.TitleFormatArgs);
+
+                var message =  uf.Mensaje.Format(uf.MensajeFormatArgs);
+
+                Notify.Show(title, message, uf.Type.ToToastType());
+            }
+            else if (value.ResultType == ReturningBase.TypeResult.Error)
+            {
+               Notify.ShowError(messageIfError,null);
+            }
+        }
+        catch (Exception)
+        {
+            // ignored
+        }
+    }
+
+    static Notify.NotificationType ToToastType(this UnfinishedInfo.NotifyType type)
+    {
+        return type switch
+               {
+                   UnfinishedInfo.NotifyType.Error      =>Notify.NotificationType.Error,
+                   UnfinishedInfo.NotifyType.Information=>Notify.NotificationType.Information,
+                   UnfinishedInfo.NotifyType.Success    =>Notify.NotificationType.Success,
+                   UnfinishedInfo.NotifyType.Warning    =>Notify.NotificationType.Warning,
+                   _                                    =>throw new ArgumentOutOfRangeException(nameof(type), type, null)
+               };
+    }
+
+}
